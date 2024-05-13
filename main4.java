3e2c8526c006b240e26591ddac499fe5e1b2fe82
bemniallani <167251364+bemniallani@users.noreply.github.com>
2024-05-13 18:01:20 +0300

    Update main4.java

--- main4.java
+++ main4.java
@@ -2,4 +2,30 @@ package question4;
 
 public class main4 {
 
+	public static void main(String[] args) {
+		char[]array= {'a','s','c','i','i'};
+		bubblesort(char array);
+		
+		for(char i: array) {
+			System.out.println(i);
+		}
+		
+	}
+
+	private static void bubblesort(char[] array) {
+		for(int i=0;i<array.length-1;i++) {
+			for(int j=0;j<array.length-i-1;j++) {
+				if(array[j]>array[j+1]) {
+					int temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+				}
+				
+			}
+			
+		}
+		
+	}
+
 }
+
